80836a9adb3d4f97c9ef979847fa48a53607b467
webegguk <info@webegg.co.uk>
2017-08-12 22:24:51 +0100

    Date range added.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {DateRangePicker} from 'react-dates';
 import logo from './logo.svg';
 import './App.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: null,
+            endDate: null,
+            focusedInput: null
+        };
+    }
+    onDatesChange({ startDate, endDate }) {
+        this.setState({ startDate, endDate });
+        if(endDate!=null){
+            console.log(startDate+', '+endDate);
+        }
+
+    }
+    onFocusChange(focusedInput) {
+        this.setState({ focusedInput });
+        //console.log(this.state.startDate);
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <DateRangePicker
+                    focused={true}
+                    withFullScreenPortal={true}
+                    numberOfMonths={2}
+                    orientation={"vertical"}
+                    withFullScreenPortal={false}
+                    isOutsideRange={() => false}
+                    startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                    endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                    onDatesChange={this.onDatesChange.bind(this)} // PropTypes.func.isRequired,
+                    focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                    onFocusChange={this.onFocusChange.bind(this)} // PropTypes.func.isRequired,
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
